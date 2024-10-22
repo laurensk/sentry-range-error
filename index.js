d51574058f7897bbc88d996cb3d8fc91e7ef446b
Laurens Kropf <laurens.kropf@kropf-it.at>
2024-10-22 09:52:46 +0200

    reproduced bug

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
  * @format
  */
 
+
+// THIS IS ALSO REQUIRED TO REPRODUCE
+import 'core-js/actual/url';
+import 'core-js/actual/url-search-params';
+
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/App';
